c92738cb5c821e715e27e7e1d408a0d4fa4ab3ae
valtallica <valerie.gutierrez@uipath.com>
2022-04-11 16:34:22 -0500

    inital

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+
+const animalSounds = ["moo", "woof", "meow", "baa"];
+const humanSounds = ["Hi", "Nee Hao", "Hola", "Hallo"];
+
+function whatDoTheySay(type) {
+  if (type === "human") {
+    console.log("This person says " + humanSounds[1]);
+  } else {
+    console.log("This animal says " + animalSounds[Math.floor(Math.random() * animalSounds.length)]);
+  }
+}
+whatDoTheySay("human");
+
